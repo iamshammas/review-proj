82f2fc8fc381101909994d0a4737a9e2220cc960
SHAMMAS <muhdshammas612@gmail.com>
2025-12-09 15:36:07 +0530

    Updated urls for the new page

--- review_system_project/urls.py
+++ review_system_project/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from newadmin import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reviews.urls')),
     path('accounts/', RedirectView.as_view(url='/', permanent=True)),
     path('newadmin/',include('newadmin.urls')),
+    path('addlist/',views.addlist,name='addlist')
 ]
