b9a0f9631d4b5ac23bde7ef89963b410f5373426
SHAMMAS <muhdshammas612@gmail.com>
2025-12-09 15:32:02 +0530

    flutter reviewers adding to model

--- newadmin/views.py
+++ newadmin/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from reviews.models import Reviewer
 # Create your views here.
 
 def register(request):
@@ -9,3 +10,17 @@ def register(request):
         user = User.objects.create_superuser(username=uname,password=pswd)
         return redirect('reg')
     return render(request,'newadmin/register.html')
+
+data = [
+        "SIYAHUDHEEN_FLUTTER",
+        "IRSHAD_FLUTTER",
+        "AKHIL_FLUTTER",
+        "AJMAL_FLUTTER",
+        "JASSIM_FLUTTER",
+        "LIKHIN_FLUTTER",
+        "JUSTINE_FLUTTER",
+    ]
+
+def addlist(request):
+    Reviewer.objects.create(name=data,stack='FLUTTER')
+    return render(request,'newadmin/test.html')
